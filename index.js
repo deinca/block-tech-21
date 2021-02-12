15d13fc3d7d31ffbb5cec04ba0f9ab2ef06f2863
deinca <zamoradhva@gmail.com>
2021-02-12 21:12:00 +0100

    mod files and adding a pug file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'); // hiermee maak ik een applicatie aan
 const loDash = require('lodash'); 
 const camelCase = require('camelcase');
+const pug = require('pug');
 
     //Hieronde maar ik een const variabel zodat 
     const messageInCamelCase = camelCase('testing-this-text-in-camel-case');
@@ -13,13 +14,22 @@ const camelCase = require('camelcase');
 const app = express();
 // In dit const variabel wordt de express framework opgeroepen en gemaakt als een module
 
-app.use('/static', express.static('static'))
+app.use('/static', express.static('static'));
+// alle files die gepubliceerd moeten worden zitten in de directory static
+
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => { 
 // vervolgens wordt een route aangemaakt
     res.send('<h1>Hallo Block Tech 2021!</h1>');
     //als de bezoeker localhost:2021/ bezoekt dan krijgt het bericht te zien.
+
+});
+
+app.get('/test', (req, res) => {
+    res.render('template',  {title: 'Hey', message: 'Hello there!' });
 
 });
 
@@ -30,6 +40,7 @@ app.get('/login', (req,res) => {
 app.get('*', (req, res) =>{
     res.send('<title>My site</title><link rel="stylesheet" href="/static/public/style.css"> <body class="bg-black fnt-white"><b>404 not fount page</b>  <p><img src="/static/images/404-cat.jpg"></p> </body>');
 });
+
 
 app.listen(port, () => {
     // we maken een locale server aan
--- static/public/functions.js
+++
@@ -1,9 +0,0 @@
-const jsonProperties = {
-    object1:{name: 'deinca', age: 13}, 
-    object2:{name: 'loquino', age: 15}
-};
-const zetDePropertiesNamenInStrings = JSON.stringify(jsonProperties); // ""
-const geefMijDePropertyValue = JSON.parse(zetDePropertiesNamenInStrings); // {"name":-"deinca"-}
-
-console.log(geefMijDePropertyValue.object1.name);
-
