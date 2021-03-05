46edc3142d88baef6b5c13ca21c188b84b6e8f9e
deinca <zamoradhva@gmail.com>
2021-03-05 06:45:14 +0100

    mod app to a little mvc

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express'); // hiermee maak ik een applicatie mee bouwen
 const slug = require('slug');
 const pug = require('pug');
 const bodyParser = require('body-parser');
-const multer  = require('multer')
+const multer  = require('multer');
 // const mongo = require('mongodb'); // geprobeerd maar duurde mij te lang
 const mongoose = require('mongoose');
 
 // Door de dotenv NPM package aante roepen kan ik codes variabelen uit de .env bestand gebruiken. 
 const dotenv = require('dotenv').config();
+
+//Hieronder roep de profile routes 
+const profileRoutes = require('./routes/profileRoutes');
+
+//Hieronder roep ik mijn profile schema aan vande models folder
+const UserConstructor = require('./models/userSchema');
 
 // In dit const variabel wordt de express framework opgeroepen en gemaakt als een module
 const app = express();
@@ -36,18 +42,6 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindA
 let jsonParser = bodyParser.json()
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-//Hieronder wordt de storage gedefineerd en hoe de naam moet worden gegenereerd
-let storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, './static/uploads/')
-    },
-    filename: function (req, file, cb)   {
-        cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
-    }
-})
-
-// Met deze variable kan multer gebruiken om de geuploade bestanden een bepaalde naam moet krijgen.
-let upload = multer({storage: storage})
 
 //Hieronder maakt ik een request functie aan waar de welkom-pagina wordt gerenderd
 app.get('/', (req, res) => {
@@ -64,100 +58,10 @@ app.get('/profile', (req,res) => {
     res.render('profile.pug', {paginaTitel: "Profiel pagina", formAction:"/profile", reqMethod:"post", reqDelete: 'delete'})
 });
 
-// Dit is mijn constructor schema model voor de gebruikers, deze schema wordt aangemaakt voor elke nieuwe object die in de database komt in collections 'users'
-const UserConstructor = mongoose.model('users', {
-    gamerAvatar: String,
-    gamerNickName: String,
-    gamerFavGame:String,
-    gamerFavChar:String,
-    uploaded: Boolean,
-});
-
-//POST = hieronder zeg ik dat objecten gemaakt kunnen worden als er bepaalde dat wordt verstuur vanuit de client-side
-app.post('/profile', upload.single('filename'), (req, res) => {
-    // maar een object aan met de ingevolde fomulier
-    const newGamer = new UserConstructor({ 
-        gamerAvatar: req.file.path,
-        gamerNickName: req.body.username,
-        gamerFavGame: req.body.game,
-        gamerFavChar: req.body.character,
-        uploaded: true
-    });
-    //Hiermee kan ik weten wat voor data wordt opgeslagen in de database
-    console.log(newGamer)
-    
-    //Nadat de data is opgeslagen in een nieuwe variabel zeg ik dat opgeslagen moet worden in de database
-    newGamer.save().then(() => {
-        //Na het opslaan geven we een redirect aan zodat we de net geuploade data weergegven wordt op de profile pagina met een ID query 
-        res.redirect(`/profile/${newGamer._id}`)
-
-        //Hiermee weet ik zeker dat de data is geupload en dat de functie goed uit wordt gevoerd 
-        console.log('A new user object is uploaded (^.^)!')
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-
-});
-
-//FIND & INSERT = hieronder zeggen we dat als er een bepaalde id wordt meegeven vanuit de client-side, 
-// dat de data gehaald wordt van uit de database uit de specifieke id
-app.get('/profile/:id', (req,res) => {
-    let id = req.params.id
-
-    UserConstructor.findById(id)
-    .then(newGamer => {
-        res.render('profile.pug', {
-        infoTitel:`Hey ${newGamer.gamerNickName}!, dit zijn je gegevens`,
-        userAva:  '../' + newGamer.gamerAvatar,
-        nameGamer: newGamer.gamerNickName,
-        favGame: newGamer.gamerFavGame,
-        favChar: newGamer.gamerFavChar,
-        uploaded: newGamer.uploaded,
-        idGamer: newGamer._id,
-        reqDelete: 'post',
-        formAction: '/profile/' + newGamer._id,
-        reqMethod: 'post'
-        }) 
-        // console.log(newGamer.gamerNickName) // mocht er iets fouts gaan dan kunnen we kijken als er echt data wordt opgehaald
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    // console.log(id) //
-});
-
-
-//UPDATE IN THE DATABASE =  
-// Ik heb gekozen om de data te laten bwereken door de client-side met een post fomulier, omdat ik de client JS wil gebruiken voor functioinaliteit in de cleintside.
-// want volgens de MDN website heb ik gezien dat je een PUT handler kan gebruiken maar dit gaat samen met de Client JS en dit leek mij niet handig omdat ik denk aan de Progessive Enhancement principe
-app.post('/profile/:id', upload.single('filename'), (req,res) => {
-    let id = req.params.id  
-    
-    UserConstructor.findByIdAndUpdate(id, {$set: {gamerAvatar: req.file.path, gamerNickName: req.body.username, gamerFavGame: req.body.game, gamerFavChar: req.body.character}}, 
-    { sort: {_id: -1},    upsert: true  }, 
-    (err, result) => {    
-        if (err) 
-        return 
-        //zodra de boevenstade velden zijn aangepast in de database, zeg ik dat de gebruiker weer terug gestuurd wordt naar zijn profiel pagina.
-        res.redirect(`/profile/${id}`)  
-        console.log(`id:${id} is updated`)
-    })
-
-})
-
-//DELETE FROM THE DATABASE = Hier heb ik ook hetzelfde toegepast wat ik bij het UPDATE heb gedaan dur ik heb niet de delete hanlder gebruikt vanwegen de Progresseive Enhancement principe.
-// Zo kunnen mensen die niet het CSS of bepaalde dunctionaliteit in de Cleint JS alsnog gebruikt maken van mijn applicatie.
-app.post('/delete/:id', (req, res)=> {
-    let id = req.params.id
-    UserConstructor.findByIdAndDelete(id,   
-        (err, result) => {    
-        if (err) 
-        return 
-
-        res.render('delete', {paginaTitel: "Delete pagina"})
-    })
-}) 
+//Hier gebruik ik de profileRoutes die in de folder routes staat. 
+//Dit maakt de index .js overzichterlijker en zouden elke router zijn eigen model js file hebben 
+//maar voor nu laat ik met deze voobeeld zien hoe het kan.
+app.use(profileRoutes);
 
 //FIND ALL = Hieronder zeg ik dat als de client naar home gaat, dat alle objecten moeten worden opgehaald vanuit de database
 // en weergegeven moeten worden in de client door de PUG egine. Hiervoor heb ik de (result) call-back gebruikt en in een variabel gezet met de naam usersData.
---
+++ models/userSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+// Dit is mijn constructor schema model voor de gebruikers, deze schema wordt aangemaakt voor elke nieuwe object die in de database komt in collections 'users'
+const UserConstructor = mongoose.model('users', {
+    gamerAvatar: String,
+    gamerNickName: String,
+    gamerFavGame:String,
+    gamerFavChar:String,
+    uploaded: Boolean,
+});
+
+module.exports = UserConstructor;
